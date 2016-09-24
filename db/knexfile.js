5624308c4e504f8c60db61be8e44413e3d4c82b4
Rohan <sarith21@gmail.com>
2016-09-24 14:13:02 -0700

    Added prod db settings

--- db/knexfile.js
+++ db/knexfile.js
@@ -9,6 +9,18 @@ module.exports = {
     migrations: {
       tableName: 'knex_migrations'
     }
+  },
+
+  prod: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      database: 'att_logger',
+      user: 'postgres'
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 
 };
